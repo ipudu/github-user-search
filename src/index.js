2928609589a1dfac8640d17a459f9bc9944b3f47
Pu Du <pudugg@gmail.com>
2020-10-26 12:52:05 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import store from './store';
+import App from './components/App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.css';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+const initalState = {};
+
+const reducer = (state = initalState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
