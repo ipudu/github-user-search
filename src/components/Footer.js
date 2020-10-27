b8bc4cbc2120f3781d16106530dfde13ea3db3a6
Pu Du <pudugg@gmail.com>
2020-10-26 23:00:10 -0500

    add github corner banner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import GithubCorner from 'react-github-corner';
 
 const Footer = () => {
   return (
     <Container className="fixed-bottom bg-light p-3 text-center" fluid>
+      <GithubCorner
+        href="https://github.com/ipudu/github-user-search"
+        bannerColor="#64CEAA"
+      />
       <h4>
-        Made with ❤️ by{' '}
-        <a href="https://github.com/ipudu/github-user-search">Pu Du</a>
+        Made with ❤️ by <a href="https://github.com/ipudu">Pu Du</a>
       </h4>
     </Container>
   );
